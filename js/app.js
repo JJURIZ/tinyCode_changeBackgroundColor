6afa2fe95f4a75928ca742d1d5b91b9fc681c569
jjuriz <jjuriz@gmail.com>
2020-04-24 14:23:58 -0400

    Validation function now outside event listener.

--- js/app.js
+++ js/app.js
@@ -3,37 +3,28 @@ let inputHexText = document.getElementById("hex_input_text"),
     resetButton = document.getElementById("reset_color"),
     displayHexText = document.getElementById("hex_text");
 
+function validateHex() {
+    var validChar = inputHexText.value;
+
+    if (validChar.length > 6) {
+        alert("Hex code cannot exceed 6 characters.");
+        return false;
+    } else if (/[^a-fA-F0-9/]/.test(inputHexText.value)) {
+        alert('Only letters A - F are permitted');
+        return false;
+    }
+};
+
 hexInputButton.addEventListener("click", function () {
-    function validateHex() {
-        var validChar = inputHexText.value;
-
-        if (validChar.length > 6) {
-            alert("Hex code cannot exceed 6 characters.");
-            return false;
-        } else if (/[^a-fA-F0-9/]/.test(inputHexText.value)) {
-            alert('Only letters A - F are permitted');
-            return false;
-        }
-    };
-
-    if (validateHex = true) {
+    let valResult = validateHex();
+    if (valResult = true) {
         let getHex = inputHexText.value.toUpperCase();
         displayHexText.innerHTML += `You have selected ${getHex}. <br>`;
         document.body.style.backgroundColor = `#${getHex}`;
-    };
-
-    resetButton.addEventListener("click", function () {
-        let resetHex = '#FFFFFF'
-        document.body.style.backgroundColor = resetHex;
-    })
-
-    //when user clicks the button do the following:
-    //Get the value from the input box -DONE
-    //confirm the value is a valid hex code -NOT DONE
-    // 0-9, A-F - DONE
-    //change background color-DONE
-    //log background colors-DONE
-    //Also, need to reduce size of image.-DONE
-    //Need to add text and header. 
+    }
 });
 
+resetButton.addEventListener("click", function () {
+    let resetHex = '#FFFFFF'
+    document.body.style.backgroundColor = resetHex;
+});
