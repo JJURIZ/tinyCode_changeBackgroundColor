e8fca0c38526485b11d0e457430a79e55397fd23
jjuriz <jjuriz@gmail.com>
2020-04-24 11:33:27 -0400

    Placed validation function inside button function.

--- js/app.js
+++ js/app.js
@@ -3,30 +3,29 @@ let inputHexText = document.getElementById("hex_input_text"),
     resetButton = document.getElementById("reset_color"),
     displayHexText = document.getElementById("hex_text");
 
-function validateHex() {
-    var validChar = inputHexText.value;
-
-    if (validChar.length > 6) {
-        alert("Value cannot exceed 6 characters.");
-        return false;
-    } else if (/[^a-fA-F0-9/]/.test(inputHexText.value)) {
-        alert('Only letters A - F are permitted');
-        return false;
-    }
-};
-
 hexInputButton.addEventListener("click", function () {
-    validateHex();
-    if (validateHex = true){
-    let getHex = inputHexText.value.toUpperCase();
-    displayHexText.innerHTML += `You have selected ${getHex}. <br>`;
-    document.body.style.backgroundColor = `#${getHex}`;
+    function validateHex() {
+        var validChar = inputHexText.value;
+
+        if (validChar.length > 6) {
+            alert("Hex code cannot exceed 6 characters.");
+            return false;
+        } else if (/[^a-fA-F0-9/]/.test(inputHexText.value)) {
+            alert('Only letters A - F are permitted');
+            return false;
+        }
     };
 
-resetButton.addEventListener("click", function(){
-    let resetHex = '#FFFFFF'
-    document.body.style.backgroundColor = resetHex;
-})
+    if (validateHex = true) {
+        let getHex = inputHexText.value.toUpperCase();
+        displayHexText.innerHTML += `You have selected ${getHex}. <br>`;
+        document.body.style.backgroundColor = `#${getHex}`;
+    };
+
+    resetButton.addEventListener("click", function () {
+        let resetHex = '#FFFFFF'
+        document.body.style.backgroundColor = resetHex;
+    })
 
     //when user clicks the button do the following:
     //Get the value from the input box -DONE
